e6c1b1caddae8547e6396288e2de359e12cf6f92
DanielSlaOq <165192946+DanielSlaOq@users.noreply.github.com>
2024-06-12 14:07:06 -0300

    Arquivos iniciais para o projeto MakeCode

---
+++ main.ts
@@ -0,0 +1,14 @@
+let a = 0
+let b = 0
+input.onButtonPressed(Button.A, function () {
+    a = randint(1, 12)
+    basic.showNumber(a)
+})
+input.onButtonPressed(Button.AB, function () {
+    b = randint(1, 12)
+    basic.showNumber(a * b)
+})
+input.onButtonPressed(Button.B, function () {
+    b = randint(1, 12)
+    basic.showNumber(b)
+})
